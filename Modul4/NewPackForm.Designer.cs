9a2799f13c196ef470e6b288bbbeaa9f0a477226
Igor Dywicki <igor.dyw@gmail.com>
2020-04-29 16:08:25 +0200

    fix

--- Modul4/NewPackForm.Designer.cs
+++ Modul4/NewPackForm.Designer.cs
@@ -65,6 +65,7 @@
             this.button1.TabIndex = 2;
             this.button1.Text = "<-";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
--- Modul4/NewPackForm.cs
+++ Modul4/NewPackForm.cs
@@ -30,6 +30,7 @@ namespace Modul4
         private void button2_Click(object sender, EventArgs e)
         {
             listBox2.Items.Add(listBox1.SelectedItem);
+            listBox1.Items.Remove(listBox1.SelectedItem);
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -46,6 +47,13 @@ namespace Modul4
         private void label2_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(listBox2.SelectedItem);
+            listBox2.Items.Remove(listBox2.SelectedItem);
+            
         }
     }
 }
