b824a32031e4fd0ebacf9d67fae7f419ba800c19
Mateusz Klepacz <mateusz.klepacz2@unilodz.eu>
2020-04-30 13:49:55 +0200

    SprawdźTo

--- Login/Login/Form1.Designer.cs
+++ Login/Login/Form1.Designer.cs
@@ -29,47 +29,45 @@
         private void InitializeComponent()
         {
             this.textBoxLogin = new System.Windows.Forms.TextBox();
-            this.labelLogin = new System.Windows.Forms.Label();
-            this.labelPassword = new System.Windows.Forms.Label();
             this.buttonLogin = new System.Windows.Forms.Button();
             this.textBoxPassword = new System.Windows.Forms.TextBox();
             this.splitter1 = new System.Windows.Forms.Splitter();
             this.splitter2 = new System.Windows.Forms.Splitter();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ButtonClearCode = new System.Windows.Forms.Button();
+            this.ButtonLogInWithCode = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // textBoxLogin
             // 
-            this.textBoxLogin.Location = new System.Drawing.Point(354, 144);
+            this.textBoxLogin.Location = new System.Drawing.Point(210, 106);
+            this.textBoxLogin.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBoxLogin.Name = "textBoxLogin";
-            this.textBoxLogin.Size = new System.Drawing.Size(151, 22);
+            this.textBoxLogin.Size = new System.Drawing.Size(144, 20);
             this.textBoxLogin.TabIndex = 0;
-            // 
-            // labelLogin
-            // 
-            this.labelLogin.AutoSize = true;
-            this.labelLogin.Location = new System.Drawing.Point(232, 147);
-            this.labelLogin.Name = "labelLogin";
-            this.labelLogin.Size = new System.Drawing.Size(55, 17);
-            this.labelLogin.TabIndex = 1;
-            this.labelLogin.Text = "LOGIN:";
-            this.labelLogin.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // labelPassword
-            // 
-            this.labelPassword.AutoSize = true;
-            this.labelPassword.Location = new System.Drawing.Point(232, 192);
-            this.labelPassword.Name = "labelPassword";
-            this.labelPassword.Size = new System.Drawing.Size(92, 17);
-            this.labelPassword.TabIndex = 2;
-            this.labelPassword.Text = "PASSWORD:";
             // 
             // buttonLogin
             // 
-            this.buttonLogin.Location = new System.Drawing.Point(299, 267);
+            this.buttonLogin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonLogin.Location = new System.Drawing.Point(187, 228);
+            this.buttonLogin.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonLogin.Name = "buttonLogin";
-            this.buttonLogin.Size = new System.Drawing.Size(116, 40);
+            this.buttonLogin.Size = new System.Drawing.Size(151, 35);
             this.buttonLogin.TabIndex = 3;
             this.buttonLogin.Text = "LOG IN";
             this.buttonLogin.UseVisualStyleBackColor = true;
@@ -77,61 +75,232 @@
             // 
             // textBoxPassword
             // 
-            this.textBoxPassword.Location = new System.Drawing.Point(354, 192);
+            this.textBoxPassword.Location = new System.Drawing.Point(210, 167);
+            this.textBoxPassword.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBoxPassword.Name = "textBoxPassword";
             this.textBoxPassword.PasswordChar = '*';
-            this.textBoxPassword.Size = new System.Drawing.Size(151, 22);
+            this.textBoxPassword.Size = new System.Drawing.Size(144, 20);
             this.textBoxPassword.TabIndex = 4;
+            this.textBoxPassword.TextChanged += new System.EventHandler(this.TextBoxPassword_TextChanged);
             // 
             // splitter1
             // 
             this.splitter1.Location = new System.Drawing.Point(0, 0);
+            this.splitter1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.splitter1.Name = "splitter1";
-            this.splitter1.Size = new System.Drawing.Size(3, 506);
+            this.splitter1.Size = new System.Drawing.Size(2, 411);
             this.splitter1.TabIndex = 5;
             this.splitter1.TabStop = false;
             // 
             // splitter2
             // 
-            this.splitter2.Location = new System.Drawing.Point(3, 0);
+            this.splitter2.Location = new System.Drawing.Point(2, 0);
+            this.splitter2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.splitter2.Name = "splitter2";
-            this.splitter2.Size = new System.Drawing.Size(3, 506);
+            this.splitter2.Size = new System.Drawing.Size(2, 411);
             this.splitter2.TabIndex = 6;
             this.splitter2.TabStop = false;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(771, 163);
+            this.textBox3.Location = new System.Drawing.Point(521, 105);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(151, 22);
+            this.textBox3.Size = new System.Drawing.Size(114, 20);
             this.textBox3.TabIndex = 7;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(654, 163);
+            this.label1.Location = new System.Drawing.Point(538, 73);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(98, 17);
+            this.label1.Size = new System.Drawing.Size(76, 13);
             this.label1.TabIndex = 8;
             this.label1.Text = "CODE LOGIN:";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(533, 130);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(31, 29);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(563, 130);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(31, 29);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(593, 130);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(31, 29);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(533, 158);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(31, 29);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(563, 158);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(31, 29);
+            this.button5.TabIndex = 15;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(593, 158);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(31, 29);
+            this.button6.TabIndex = 16;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.Button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(533, 186);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(31, 29);
+            this.button7.TabIndex = 17;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.Button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(563, 186);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(31, 29);
+            this.button8.TabIndex = 18;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.Button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(593, 186);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(31, 29);
+            this.button9.TabIndex = 19;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.Button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(533, 214);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(91, 25);
+            this.button10.TabIndex = 20;
+            this.button10.Text = "0";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.Button10_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.BackgroundImage = global::Login.Properties.Resources.lockpass;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(159, 158);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(35, 36);
+            this.pictureBox2.TabIndex = 22;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BackgroundImage = global::Login.Properties.Resources.userNew;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Image = global::Login.Properties.Resources.userNew;
+            this.pictureBox1.Location = new System.Drawing.Point(159, 100);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(35, 37);
+            this.pictureBox1.TabIndex = 21;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ButtonClearCode
+            // 
+            this.ButtonClearCode.BackColor = System.Drawing.Color.White;
+            this.ButtonClearCode.BackgroundImage = global::Login.Properties.Resources.close;
+            this.ButtonClearCode.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ButtonClearCode.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ButtonClearCode.Location = new System.Drawing.Point(571, 269);
+            this.ButtonClearCode.Name = "ButtonClearCode";
+            this.ButtonClearCode.Size = new System.Drawing.Size(39, 37);
+            this.ButtonClearCode.TabIndex = 10;
+            this.ButtonClearCode.UseVisualStyleBackColor = false;
+            this.ButtonClearCode.Click += new System.EventHandler(this.ButtonClearCode_Click);
+            // 
+            // ButtonLogInWithCode
+            // 
+            this.ButtonLogInWithCode.BackColor = System.Drawing.Color.White;
+            this.ButtonLogInWithCode.BackgroundImage = global::Login.Properties.Resources.signs;
+            this.ButtonLogInWithCode.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ButtonLogInWithCode.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ButtonLogInWithCode.Location = new System.Drawing.Point(526, 269);
+            this.ButtonLogInWithCode.Name = "ButtonLogInWithCode";
+            this.ButtonLogInWithCode.Size = new System.Drawing.Size(39, 37);
+            this.ButtonLogInWithCode.TabIndex = 9;
+            this.ButtonLogInWithCode.UseVisualStyleBackColor = false;
+            this.ButtonLogInWithCode.Click += new System.EventHandler(this.ButtonLogInWithCode_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(983, 506);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(737, 411);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ButtonClearCode);
+            this.Controls.Add(this.ButtonLogInWithCode);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.splitter2);
             this.Controls.Add(this.splitter1);
             this.Controls.Add(this.textBoxPassword);
             this.Controls.Add(this.buttonLogin);
-            this.Controls.Add(this.labelPassword);
-            this.Controls.Add(this.labelLogin);
             this.Controls.Add(this.textBoxLogin);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -140,14 +309,26 @@
         #endregion
 
         private System.Windows.Forms.TextBox textBoxLogin;
-        private System.Windows.Forms.Label labelLogin;
-        private System.Windows.Forms.Label labelPassword;
         private System.Windows.Forms.Button buttonLogin;
         private System.Windows.Forms.TextBox textBoxPassword;
         private System.Windows.Forms.Splitter splitter1;
         private System.Windows.Forms.Splitter splitter2;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ButtonLogInWithCode;
+        private System.Windows.Forms.Button ButtonClearCode;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
--- Login/Login/Form1.cs
+++ Login/Login/Form1.cs
@@ -18,13 +18,13 @@ namespace Login
         public Form1()
         {
             InitializeComponent();
-            
+
         }
 
-        string connection = "Server=tcp:kinosql.database.windows.net,1433;Initial Catalog=modul_logowania_gr1;Persist Security Info=False;User ID=student;Password=Pa$$w0rd;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        string connection = "Server=tcp:kinosql.database.windows.net,1433;Initial Catalog=kino;Persist Security Info=False;User ID=student;Password=Pa$$w0rd;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
         private void Form1_Load(object sender, EventArgs e)
         {
-            
+
         }
 
         public static byte[] StringHash(string str)
@@ -32,7 +32,7 @@ namespace Login
             HashAlgorithm algorithm = SHA1.Create();
             return algorithm.ComputeHash(Encoding.Unicode.GetBytes(str));
         }
-        
+
 
 
         private void label1_Click(object sender, EventArgs e)
@@ -53,14 +53,15 @@ namespace Login
                 }
                 else
                 {
-                    
+
                     SqlConnection con = new SqlConnection(connection);
 
-                    SqlCommand cmd = new SqlCommand("SELECT * FROM dbo.uzytkownicy WHERE login=@Login AND haslo=@haslo;", con);
+                    SqlCommand cmd = new SqlCommand("SELECT * FROM dbo.User WHERE login=@Login AND password=@password;", con);
                     cmd.Parameters.AddWithValue("@Login", textBoxLogin.Text);
-                    cmd.Parameters.AddWithValue("@haslo", StringHash(textBoxPassword.Text));
+                    cmd.Parameters.AddWithValue("@password", StringHash(textBoxPassword.Text));
 
-                    con.Open();   
+
+                    con.Open();
                     SqlDataAdapter da = new SqlDataAdapter(cmd);
                     DataSet ds = new DataSet();
                     da.Fill(ds);
@@ -68,23 +69,133 @@ namespace Login
 
                     int count = ds.Tables[0].Rows.Count;
 
-                    if (count == 1 )
-                         {
-                        MessageBox.Show("Poprawnie zalogowano użytkownika.");
-                        }
-                     else
-                         {
-                        MessageBox.Show("Błąd logowania, wprowadź ponownie dane.");
-                        }
-                    
+                    if (count == 1)
+                    {
+                        MessageBox.Show("Successful.");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Login failed. Check your login and password and try again.");
+                    }
+
                 }
 
-             }
+            }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.ToString());
             }
-                #endregion
+            #endregion
+        }
+
+
+        private void ButtonLogInWithCode_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                if (string.IsNullOrWhiteSpace(textBox3.Text))
+                {
+                    MessageBox.Show("Enter your code");
+                    textBox3.Focus();
+                    return;
+                }
+                else
+                {
+
+                    SqlConnection con = new SqlConnection(connection);
+
+                    SqlCommand cmd = new SqlCommand("SELECT * FROM dbo.User WHERE code=@Code;", con);
+                    cmd.Parameters.AddWithValue("@Code", StringHash(textBox3.Text));
+
+                    con.Open();
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataSet ds = new DataSet();
+                    da.Fill(ds);
+                    con.Close();
+
+                    int count = ds.Tables[0].Rows.Count;
+
+                    if (count == 1)
+                    {
+                        MessageBox.Show("Successful.");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Login failed. Check your code and try again.");
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+
+        }
+
+        #region NumericKeyboard
+        private void Button1_Click_1(object sender, EventArgs e)
+        {
+            textBox3.Text = textBox3.Text + "1";
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = textBox3.Text + "2";
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = textBox3.Text + "3";
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = textBox3.Text + "4";
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = textBox3.Text + "5";
+        }
+
+        private void Button6_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = textBox3.Text + "6";
+        }
+
+        private void Button7_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = textBox3.Text + "7";
+        }
+
+        private void Button8_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = textBox3.Text + "8";
+        }
+
+        private void Button9_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = textBox3.Text + "9";
+        }
+
+        private void Button10_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = textBox3.Text + "0";
+        }
+        #endregion
+
+        private void ButtonClearCode_Click(object sender, EventArgs e)
+        {
+            textBox3.Clear();
+        }
+
+        private void TextBoxPassword_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
