ca74fba9ea2f4675dcb2678e40701e8697aeb187
Michal <zulfoxus@gmail.com>
2020-05-07 16:09:33 +0200

    v3
    Generowanie tabel na podstawie datatable.
    Nowa klasa pdf.

---
+++ Modul_raportow/Modul_raportow/Pdf.cs
@@ -0,0 +1,30 @@
+﻿using iText.Layout.Element;
+using iText.Layout.Properties;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul_raportow
+{
+    class Pdf
+    {
+        public static Table toTable(DataTable data)
+        {
+            Table res = new Table(data.Columns.Count);
+            res.SetFontSize(10);
+            foreach (DataRow row in data.Rows)
+            {
+                for(int i = 0; i < data.Columns.Count; i++)
+                {
+                    res.AddCell(new Paragraph(row[i].ToString()));
+                }
+
+
+            }
+            return res;
+        }
+    }
+}
--- Modul_raportow/Modul_raportow/ReportGenerator.cs
+++ Modul_raportow/Modul_raportow/ReportGenerator.cs
@@ -8,12 +8,24 @@ using System.Data;
 using iText.Kernel.Pdf;
 using iText.Layout.Element;
 using iText.Layout;
+using iText.Kernel.Geom;
 
 namespace Modul_raportow
 {
     class ReportGenerator
     {
-        public static void generateAllMoviesReport() { }
+        static DataTable res;
+        static string exportFolder = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        static string exportFile = System.IO.Path.Combine(exportFolder, "test1.pdf");
+
+        public static void generateAllMoviesReport()
+        {
+            string zapytanie = " SELECT dbo.Movie.title, dbo.Movie.description, dbo.MovieState.name, dbo.MovieType.name, dbo.Movie.movietime  FROM dbo.Movie INNER JOIN dbo.MovieState ON  dbo.Movie.movieState=dbo.MovieState.id INNER JOIN dbo.MovieType ON dbo.Movie.movieType = dbo.MovieType.id";
+
+            res = SQLObject.SendCommand(zapytanie);
+
+
+        }
 
         public static void generateSalariesReport(DateTime dateFrom, DateTime dateTo) { }
         public static void generateWorkTimeReport(DateTime dateFrom, DateTime dateTo) { }
@@ -28,10 +40,9 @@ namespace Modul_raportow
 
         public static void Raport_podsumowania_czasu_pracy_pracownikow()
         {
-            DataTable res;
-            var exportFolder = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-            var exportFile = System.IO.Path.Combine(exportFolder, "test1.pdf");
-            string zapytanie = "SELECT dbo.Schedule.\"User\"Id AS Identryfikator,dbo.\"User\".firstname AS Imie, dbo.\"User\".lastname AS Nazwisko,CONVERT(TIME, DATEADD(s, SUM(( DATEPART(hh, CAST(dbo.Schedule.dateTo - dbo.Schedule.dateFrom AS Time(0))) * 3600 ) + ( DATEPART(mi, CAST(dbo.Schedule.dateTo - dbo.Schedule.dateFrom AS Time(0))) * 60 ) + DATEPART(ss, CAST(dbo.Schedule.dateTo - dbo.Schedule.dateFrom AS Time(0)))), 0)) AS \"Czas pracy\" FROM dbo.Schedule INNER JOIN dbo.\"User\" ON dbo.\"User\".id=dbo.Schedule.\"User\"Id GROUP BY dbo.Schedule.\"User\"Id, dbo.\"User\".firstname, dbo.\"User\".lastname;";
+            res = null;
+
+            string zapytanie = "SELECT dbo.Movie.id, dbo.Movie.title, dbo.Movie.description, dbo.MovieState.name, dbo.MovieType.name, dbo.Movie.movietime  FROM dbo.Movie INNER JOIN dbo.MovieState ON  dbo.Movie.movieState=dbo.MovieState.id INNER JOIN dbo.MovieType ON dbo.Movie.movieType = dbo.MovieType.id";
 
             res = SQLObject.SendCommand(zapytanie);
 
@@ -42,10 +53,7 @@ namespace Modul_raportow
                 using (var pdf = new PdfDocument(writer))
                 {
                     var doc = new Document(pdf);
-                    foreach (DataRow row in res.Rows)
-                    {
-                        doc.Add(new Paragraph(row[0].ToString()));
-                    }
+                    doc.Add(Pdf.toTable(res));
                     
                 }
             }
