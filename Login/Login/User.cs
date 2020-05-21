2f34d5704b9706e283ecd4bd06ae30ebe6f86a8e
Piotr Cisowski <piotr.cisowski@unilodz.eu>
2020-05-21 11:17:55 +0200

    add permission checker

--- Login/Login/User.cs
+++ Login/Login/User.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Login
 {
     class User
     {
-        List<string> PermissionList = new List<string>();
+        private List<string> PermissionList = new List<string>();
 
         int id;
       
@@ -21,6 +22,16 @@ namespace Login
             PermissionList.Add(permission);
             return permission;
         }
-    
+        public string PrintListOfPermission()
+        {
+            string x = "nic";
+            if (PermissionList.Count != 0)
+            {
+                foreach (string permission in PermissionList)
+                    return permission;
+            }
+            return x;
+
+        }
     }
 }
